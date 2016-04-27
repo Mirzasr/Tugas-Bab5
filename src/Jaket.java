35a885690ecedf495177b1e4eaedd5a1eb4a5cbf
Mirza Syauqi Rafif <mirzasrafif@yahoo.com>
2016-04-27 14:17:19 +0700

    tambah class jaket
    deklarasi atribut

---
+++ src/Jaket.java
@@ -0,0 +1,12 @@
+
+public class Jaket {
+
+    public String produk[] = {"Jaket A", "Jaket B", "Jaket C"};
+    public final int hargaSatuan[] = {100000, 125000, 175000};
+    public final int diskon[] = {5000, 5000, 15000};
+    public int jumlah[];
+    public int harga[] = new int[3];
+    public int total = 0;
+
+
+}
